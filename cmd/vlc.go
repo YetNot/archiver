a342f595b72ba54cfc0298b7f4317cfc7679f956
Kirill Glinskikh <glinskix24@bk.ru>
2024-01-30 05:34:24 +0300

    сжатие текста

--- cmd/vlc.go
+++ cmd/vlc.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"archiver/lib/vlc"
+	"errors"
 	"io"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -13,26 +17,42 @@ var vlcCmd = &cobra.Command{
 	Run:   pack,
 }
 
+const packedExtension = ".vlc"
+
+var ErrEmptyPath = errors.New("path to file is not specified")
+
 func pack(_ *cobra.Command, args []string) {
+	if len(args) == 0 || args[0] == "" {
+		handleError(ErrEmptyPath)
+	}
+
 	filePath := args[0]
 
 	r, err := os.Open(filePath)
 	if err != nil {
 		handleError(err)
 	}
+	defer r.Close()
 
 	data, err := io.ReadAll(r)
 	if err != nil {
 		handleError(err)
 	}
 
-	// packed := Encode(data)
-	packed := ""
+	packed := vlc.Encode(string(data))
 
-	err = io.WriteFile(packedFileName(), []byte(packed), 0644)
+	err = os.WriteFile(packedFileName(filePath), []byte(packed), 0644)
 	if err != nil {
 		handleError(err)
 	}
 }
 
-func packedFileName()
+func packedFileName(path string) string {
+	fileName := filepath.Base(path)
+
+	return strings.TrimSuffix(fileName, filepath.Ext(fileName)) + packedExtension
+}
+
+func init() {
+	packCmd.AddCommand(vlcCmd)
+}
